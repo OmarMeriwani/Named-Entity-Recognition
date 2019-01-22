3ebbe3b8f3ab8d118fe3dc4f896684dbaa2def77
OmarMeriwani <omar.sirwan@gmail.com>
2019-01-22 23:51:56 +0000

    --

--- SpacySolution.py
+++ SpacySolution.py
@@ -1,8 +1,8 @@
 import spacy
 from spacy import displacy
 from collections import Counter
-import en_core_web_sm
-nlp = en_core_web_sm.load()
+#import en_core_web_sm
+nlp = spacy.load('en')
 from pprint import pprint
 
 doc = nlp('European authorities fined Google a record $5.1 billion on Wednesday for abusing its power in the mobile phone market and ordered the company to alter its practices')
